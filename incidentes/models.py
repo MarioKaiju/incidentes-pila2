a9c7b9f945087494db66446807213a89e8e0c9c1
Mario Alberto Vázquez Anaya <marioavazquez2011@hotmail.com>
2022-11-17 23:21:13 -0600

    Modelo incidente

--- incidentes/models.py
+++ incidentes/models.py
@@ -1,3 +1,24 @@
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
+class Incidente(models.Model):
+  ASEGURAMIENTO = (
+        ('PT', 'Perdida total'),
+        ('DC', 'Daños colaterales'),
+        ('DP', 'Daños parciales'),
+    )
+  id_vehiculo = models.BigAutoField(primary_key=True)
+  matricula = models.CharField(max_length=10, unique=True)
+  modelo = models.TextField()
+  propietario = models.ForeignKey(
+    'auth.User',
+    on_delete=models.CASCADE
+  )
+  aseguramiento = models.CharField(max_length=2, choices=ASEGURAMIENTO)
+
+  def __str__(self):
+    return self.modelo + self.propietario
+
+  def get_absolute_url(self):
+    return reverse('detalle_incidente', args=[str(self.id_vehiculo)])
--- incidentes/views.py
+++ incidentes/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Incidente
 
 # Create your views here.
-class VistaListaIncidentes(TemplateView):
+class VistaListaIncidentes(ListView):
+  model= Incidente
   template_name = 'inicio.html'
