b153a4343b277cc5da772f8a181014a9d14ef9b7
Mario Alberto Vázquez Anaya <marioavazquez2011@hotmail.com>
2022-11-17 22:24:04 -0600

    Archivos estaticos

--- config/settings.py
+++ config/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/arch/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('assets'))]
-#STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('v1/assets'))
+STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('archivosestaticos'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
