17885e520e5d5747d30b16bb32b2f59581e42bce
Mario Alberto Vázquez Anaya <marioavazquez2011@hotmail.com>
2022-11-17 22:10:56 -0600

    Plantillas de inicio añadidas

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('assets'))]
-#STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('assets'))
+STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('v1/assets'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
